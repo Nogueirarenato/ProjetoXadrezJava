71482c54f388be45c1095ddb5a9798e787481515
Renato Nogueira da Silva <renato1987geofisica@outlook.com>
2022-01-31 18:42:32 -0300

    BoardException and defensive programming

--- Chess/src/chess/ChessMatch.java
+++ Chess/src/chess/ChessMatch.java
@@ -30,5 +30,6 @@ public class ChessMatch {
 		board.placePiece(new Rook(board, Color.BLACK), new Position(2,1));
 		board.placePiece(new King(board, Color.WHITE), new Position(0,4));
 		board.placePiece(new Rook(board, Color.BLACK), new Position(3,1));
+		
 	}
 }
